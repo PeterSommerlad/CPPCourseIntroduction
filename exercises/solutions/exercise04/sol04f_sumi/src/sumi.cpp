34a0d5c0ba3478e18d244a5c49b9d4b2111b3345
PeterSommerlad <peter.cpp@sommerlad.ch>
2022-03-07 14:25:06 +0100

    cleaning up exercise 03

--- exercises/solutions/exercise04/sol04f_sumi/src/sumi.cpp
+++
@@ -1,9 +0,0 @@
-#include "sumi.h"
-#include <istream>
-#include <iterator>
-#include <numeric>
-int sumi(std::istream &is) {
-	using input=std::istream_iterator<int>;
-	input eof{};
-	return std::reduce(input{is},eof,0);
-}
